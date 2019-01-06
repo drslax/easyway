a52446e557dd37715d81c38d0be603b4818663bc
aelouarg <anas.elouargui@gmail.com>
2019-01-06 19:59:39 +0100

    first try

---
+++ dataWriter.py
@@ -0,0 +1,70 @@
+import os
+import datetime
+import xlsxwriter as xls
+
+
+def print_result(dic):
+
+# Open files:
+
+    rep = datetime.datetime.now().strftime("%Y_%b_%d")
+    fich = datetime.datetime.now().strftime("%l%p")
+
+    access_rights = 0o755
+    if not os.path.exists("%s" % rep):
+        os.makedirs("%s" % rep, access_rights)
+
+# Create the Book:
+    wBook = xls.Workbook("./%s/file_%s.xlsx" % (rep, fich))
+
+# Create the Sheet:
+    wSheet = wBook.add_worksheet()
+
+# Set cases:
+    wSheet.set_column('A:Z', 35)
+    wSheet.set_row('0:50', 30)
+
+# Format in merge cases:
+    m_format = wBook.add_format({
+        'bold': 1,
+        'border': 1,
+        'border_color' : 'blue',
+        'align': 'center',
+        'valign': 'vcenter',
+        'fg_color': '#00ffbf'})
+
+# Merge cells:
+    wSheet.merge_range('B1:I1', datetime.datetime.now().strftime("%A %B %Y    %X %z"), m_format)
+    wSheet.merge_range('D3:I3', 'Prix', m_format)
+
+
+# Start from the first cell and write data:
+    tab = ['Depart', 'Destination', 'Lecab', 'Uber Green', 'Uber UberX', 'Uber berline', 'Uber Van', 'Uber ACCESS']
+
+    row = 3
+    col = 1
+    for item in tab:
+        wSheet.write(row, col, tab[col-1], m_format)
+        col +=1
+    for i in range(0,10):
+        col = 1
+        wSheet.write(row, col, dic[i]["Depart"], m_format)
+        col += 1
+        wSheet.write(row, col, dic[i]["Destination"], m_format)
+        col += 1
+        wSheet.write(row, col, dic[i]["Lecab"], m_format)
+        col += 1
+        wSheet.write(row, col, dic[i]["Green"], m_format)
+        col += 1
+        wSheet.write(row, col, dic[i]["UberX"], m_format)
+        col += 1
+        wSheet.write(row, col, dic[i]["Berline"], m_format)
+        col += 1
+        wSheet.write(row, col, dic[i]["Van"], m_format)
+        col += 1
+        wSheet.write(row, col, dic[i]["ACCESS"], m_format)
+        row += 1
+
+
+# Close the book
+    wBook.close()
